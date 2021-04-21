27f5885363ff881be2fd21dd03d9955f35c646b5
adhyuva <adhyuva8@gmail.com>
2021-04-20 21:46:52 -0400

    Day 3

--- data.js
+++ data.js
@@ -1,81 +1,31 @@
 let data = [
     {
-        answer: "H",
         questionAndPoints: [
             {
-                question:"I was used in many airships including the Hindeburg.",
-                points:15
+                question:"Number or electrons in Hydrogen",
+                answer:1
             },{
-                question:"The Sun burns these atoms.",
-                points:10
+                question:" Hydrogen's State room temperature",
+                answer:"Gas"
             },
             {
-                question:"Hydrogen abbravation",
-                points:5
+                question:"Hydrogen's abbravation",
+                answer:"H"
             }
         ]
     },
     {
-        answer: "He",
         questionAndPoints: [
             {
-                question:"When current runs through _______ it gives a pinksih glow!",
-                points:15
+                question:"Number or electrons in Helium",
+                answer:2
             },{
-                question:"I am used in birthday ballons!",
-                points:10
+                question:" Helium's State room temperature",
+                answer:"Gas"
             },
             {
                 question:"Helium's abbravation",
-                points:5
-            }
-        ]
-    },
-    {
-        answer: "Li",
-        questionAndPoints: [
-            {
-                question:"Me and sodium are the only metals that can float on water!",
-                points:15
-            },{
-                question:"I am the first alkali metals!",
-                points:10
-            },
-            {
-                question:"lithium's abbravation",
-                points:5
-            }
-        ]
-    },
-    {
-        answer: "Be",
-        questionAndPoints: [
-            {
-                question:"This element is transparent to x-rays",
-                points:15
-            },{
-                question:"A form of me is beryl",
-                points:10
-            },
-            {
-                question:"Beryllium's abbravation",
-                points:5
-            }
-        ]
-    },
-    {
-        answer: "B",
-        questionAndPoints: [
-            {
-                question:"This element is used in Silly Putty!",
-                points:15
-            },{
-                question:"I am most commonly found in borax",
-                points:10
-            },
-            {
-                question:"Boron's abbravation",
-                points:5
+                answer:"He"
             }
         ]
     },
--- main.js
+++ main.js
@@ -1,15 +1,24 @@
-let rand = Math.floor(Math.random() * data.length);
-console.log(rand);
-let questionNum = 0;
 document.getElementById("question").innerHTML = "Click 'next question' for your questions to start appering";
+let functionRun;
 function nextQuestion () {
+    functionRun = 0;
+    if(functionRun > 0){
+        console.log("g");
+        if (answer == document.getElementById("submit")) {
+            document.getElementById("result").innerHTML = "Correct";
+    
+        }else{
+            document.getElementById("result").innerHTML = "Incorrect";
+        }
+    }
+    let rand = Math.floor(Math.random() * data.length);
+    console.log(rand);
+    let questionNum = Math.floor(Math.random() * data[0].questionAndPoints.length);
     console.log(questionNum);
     let randQuestionAndPoints = data[rand].questionAndPoints;
-    let question = randQuestionAndPoints[questionNum].question
-    if(question >= randQuestionAndPoints.length){
-        rand = Math.floor(Math.random() * data.length);
-    }
-    document.getElementById("question").innerHTML = question+ " :: " + randQuestionAndPoints[questionNum].points ;
-    questionNum = questionNum + 1;
-    console.log(`At the end ${questionNum}`)
+    let question = randQuestionAndPoints[questionNum].question;
+    let answer = randQuestionAndPoints[questionNum].answer;
+    document.getElementById("question").innerHTML = `${question}<br/><br/> Answer is: ${answer}`;
+    functionRun++;
+    
 }
