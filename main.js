2483f9e3dad158cfc7506d5b6c19d29d1405a480
adhyuva <adhyuva8@gmail.com>
2021-05-09 12:11:00 -0400

    Adding 'class' to my webpage

--- main.js
+++ main.js
@@ -12,69 +12,73 @@ let points = 0;
 function nextQuestion () {
     inputElement.style.display = "block";
     if(functionRun > 0){
-        if(isNaN(answer)){
-            if (answer.toLowerCase() == document.getElementById("input").value.toLowerCase()) {
-                document.getElementById("result").innerHTML = " You got the last question correct!";
-                    console.log(functionRun);
-                    points++;
-                    if(data[rand].points < 3){
-                        data[rand].points++;
-                    }
-                    
-                    let idName = "el" + (rand+1);
-                    let AidName = idName + "a"
-                    console.log(idName)
-                    console.log(data[rand].points + " On..." + (rand + 1));
-                    let element = document.getElementById(idName);
-                    element.style.visibility = "visible";
-                    document.getElementById(AidName).style.visibility = "hidden";
-                    document.getElementById("points").innerHTML = `<b> You have ${points} point(s)!</b>`;
-            }else{
-                document.getElementById("result").innerHTML = " You got the last question incorrect. Sorry, the answer is:" + answer;
-                let idName = "el" + (rand+1);
-                    let AidName = idName + "a"
-                    let element = document.getElementById(idName);
-                    element.style.visibility = "hidden";
-                    document.getElementById(AidName).style.visibility = "visible";
-                    console.log(element.style.visibility)
-                if(data[rand].points > 0){
-                    points--;
-                    data[rand].points --;
-                    console.log(data[rand].points);
-                }
-            }
-            document.getElementById("input").value = "";
+        if(document.getElementById("input").value.toLowerCase() == ""){
+            document.getElementById("result").innerHTML = "Uh-oh! You did not write anything for the last question!";
         }else{
-            if (answer == document.getElementById("input").value) {
-                document.getElementById("result").innerHTML = " You got the last question correct!";
-                    console.log(functionRun);
-                    points++;
-                    if(data[rand].points < 3){
-                        data[rand].points++;
-                    }
+            if(isNaN(answer)){
+                if (answer.toLowerCase() == document.getElementById("input").value.toLowerCase()) {
+                    document.getElementById("result").innerHTML = " You got the last question correct!";
+                        console.log(functionRun);
+                        points++;
+                        if(data[rand].points < 3){
+                            data[rand].points++;
+                        }
+                        
+                        let idName = "el" + (rand+1);
+                        let AidName = idName + "a"
+                        console.log(idName)
+                        console.log(data[rand].points + " On..." + (rand + 1));
+                        let element = document.getElementById(idName);
+                        element.style.visibility = "visible";
+                        document.getElementById(AidName).style.visibility = "hidden";
+                        document.getElementById("points").innerHTML = `<b> You have ${points} point(s)!</b>`;
+                }else{
+                    document.getElementById("result").innerHTML = " You got the last question incorrect. The answer is:" + answer;
                     let idName = "el" + (rand+1);
-                    let AidName = idName + "a"
-                    console.log(idName)
-                    console.log(data[rand].points + " On..." + (rand + 1));
-                    let element = document.getElementById(idName);
-                    element.style.visibility = "visible";
-                    document.getElementById(AidName).style.visibility = "hidden";
-                    document.getElementById("points").innerHTML = `<b> You have ${points} point(s)!</b>`;
-            }else{
-                document.getElementById("result").innerHTML = " You got the last question incorrect. Sorry, the answer is:" + answer;
-                let idName = "el" + (rand+1);
-                    let AidName = idName + "a"
-                    let element = document.getElementById(idName);
-                    element.style.visibility = "hidden";
-                    document.getElementById(AidName).style.visibility = "visible";
-                    console.log(element.style.visibility)
-                if(data[rand].points > 0){
-                    points--;
-                    data[rand].points --;
-                    console.log(data[rand].points);
+                        let AidName = idName + "a"
+                        let element = document.getElementById(idName);
+                        element.style.visibility = "hidden";
+                        document.getElementById(AidName).style.visibility = "visible";
+                        console.log(element.style.visibility)
+                    if(data[rand].points > 0){
+                        points--;
+                        data[rand].points --;
+                        console.log(data[rand].points);
+                    }
                 }
+                document.getElementById("input").value = "";
+            }else{
+                if (answer == document.getElementById("input").value) {
+                    document.getElementById("result").innerHTML = " You got the last question correct!";
+                        console.log(functionRun);
+                        points++;
+                        if(data[rand].points < 3){
+                            data[rand].points++;
+                        }
+                        let idName = "el" + (rand+1);
+                        let AidName = idName + "a"
+                        console.log(idName)
+                        console.log(data[rand].points + " On..." + (rand + 1));
+                        let element = document.getElementById(idName);
+                        element.style.visibility = "visible";
+                        document.getElementById(AidName).style.visibility = "hidden";
+                        document.getElementById("points").innerHTML = `<b> You have ${points} point(s)!</b>`;
+                }else{
+                    document.getElementById("result").innerHTML = " You got the last question incorrect. The answer is:" + answer;
+                    let idName = "el" + (rand+1);
+                        let AidName = idName + "a"
+                        let element = document.getElementById(idName);
+                        element.style.visibility = "hidden";
+                        document.getElementById(AidName).style.visibility = "visible";
+                        console.log(element.style.visibility)
+                    if(data[rand].points > 0){
+                        points--;
+                        data[rand].points --;
+                        console.log(data[rand].points);
+                    }
+                }
+                document.getElementById("input").value = ""; 
             }
-            document.getElementById("input").value = ""; 
         }
     }
     questionNum = Math.floor(Math.random() * data[0].questionAndPoints.length);
@@ -87,15 +91,4 @@ function nextQuestion () {
     document.getElementById("question").innerHTML = `${question}`;
     functionRun++;
     
-}
-
-function showRules () {
-    document.getElementById("rules").style.display = "block"
-    document.getElementById("showRules").style.display = "none"
-    document.getElementById("showRules").style.display = "block"
-}
-function hideRules () {
-    document.getElementById("rules").style.display = "none"
-    document.getElementById("hideRules").style.display = "block"
-    document.getElementById("hideRules").style.display = "block"
 }
--- topbar.js
+++ topbar.js
@@ -7,20 +7,30 @@ let quiz = $("#quiz");
 let progress = $("#progress");
 quiz.hide()
 progress.hide()
+ruleBtn.addClass("onTab")
 function showRules () {
     rule.show()
     quiz.hide()
     progress.hide()
+    ruleBtn.addClass("onTab").removeClass("topBtn")
+    quizBtn.removeClass("onTab").addClass("topBtn")
+    progressBtn.removeClass("onTab").addClass("topBtn")
 }
 function showQuiz () {
     quiz.show()
     progress.hide()
     rule.hide()
+    quizBtn.addClass("onTab").removeClass("topBtn")
+    ruleBtn.removeClass("onTab").addClass("topBtn")
+    progressBtn.removeClass("onTab").addClass("topBtn")
 }
 function showProgress () {
     progress.show()
     quiz.hide()
     rule.hide()
+    quizBtn.removeClass("onTab").addClass("topBtn")
+    ruleBtn.removeClass("onTab").addClass("topBtn")
+    progressBtn.addClass("onTab").removeClass("topBtn")
 }
 ruleBtn.on("click",function(){showRules()})
 quizBtn.on("click",function(){showQuiz()})
